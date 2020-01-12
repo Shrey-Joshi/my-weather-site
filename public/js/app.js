64ab42befbc7a68d5bef5c3b3bfd6cd3a970a0dd
Dpmon1 <shreyjoshi3103@gmail.com>
2020-01-12 18:34:55 -0500

    Fixed bug with some lines not appearing.

--- public/js/app.js
+++ public/js/app.js
@@ -4,13 +4,21 @@ const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
+const messageFour = document.querySelector('#message-4')
+const messageFive = document.querySelector('#message-5')
+const messageSix = document.querySelector('#message-6')
 
 weatherForm.addEventListener('submit', e => {
     e.preventDefault()
     const location = searchElement.value
 
-    messageOne.textcontent = "Loading..."
-    messageOne.textcontent = "--"
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = ""
+    messageThree.textContent = ""
+    messageFour.textContent = ""
+    messageFive.textContent = ""
+    messageSix.textContent = ""
 
     fetch('/weather?address='+location).then((response) => {
         response.json().then((data) => {
@@ -20,7 +28,11 @@ weatherForm.addEventListener('submit', e => {
                 console.log(data.error)
             } else {
                 messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
+                messageTwo.textContent = data.forecast[0]
+                messageThree.textContent = data.forecast[1]
+                messageFour.textContent = data.forecast[2]
+                messageFive.textContent = data.forecast[3]
+                messageSix.textContent = data.forecast[4]
                 console.log(data.location)
                 console.log(data.forecast)
             }
