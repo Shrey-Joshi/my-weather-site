3e95568dc566903f6f84df14b7061e1d0847cac0
Dpmon1 <shreyjoshi3103@gmail.com>
2021-03-17 19:53:52 -0400

    Fixed floating point error in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,14 +11,19 @@ const forecast = (latitude, longitude, callback) => {
         } else {
             callback(undefined, [
                 body.daily.data[0].summary, 
-                " It is currently " + body.currently.temperature + 
-                " degrees out. There is a " + body.currently.precipProbability + 
+                " It is currently " + (body.currently.temperature).toPrecision(3) + 
+                " degrees out. There is a " + (body.currently.precipProbability).toPrecision(1) + 
                 "% chance of rain. ", 
                 "We expect there to be: " + body.daily.summary, 
-                "The expected temperature high will be " + body.daily.data[0].temperatureHigh + 
-                " and the expected temperature low will be " + body.daily.data[0].temperatureLow + 
+
+                "The expected temperature high will be " + 
+                (body.daily.data[0].temperatureHigh).toPrecision(3) + 
+
+                " and the expected temperature low will be " + 
+                (body.daily.data[0].temperatureLow).toPrecision(3) + 
+
                 ".", 
-                "Humidity will be " + (100*body.daily.data[0].humidity) + 
+                "Humidity will be " + (100*body.daily.data[0].humidity).toPrecision(3) + 
                 " and the UV index will be " + body.daily.data[0].uvIndex + 
                 "."
             ])
