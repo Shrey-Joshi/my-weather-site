d6015ac21564211132feca92d6e627d4b53de270
Dpmon1 <shreyjoshi3103@gmail.com>
2021-03-18 17:29:13 -0400

    Fixed Security issue in Geocode

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,32 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
+	// Ensure the address is proper:
+	// Properly encoding:
+	address = encodeURIComponent(address)
+
+	// Lack of semicolons:
+	// If any then remove them
+	address = address.replaceAll("%3B", "")
+
+	// Only UTF-8 chars
+
+	// <= 20 words:
+	if (address.split("+").length > 20){
+		// If so then remove the rest of the words
+		address = ((address.split("+")).slice(0, 20)).join("+")
+	} 
+
+	// <= 256 characters
+	if (address.length > 256) {
+		address = address.slice(0, 256)
+	}
+
+	// If there are no characters left in string:
+	if (address.length === 0) {
+		callback('Invalid input.', undefined)
+	}
+
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoidHJpY2t5LWN1dGxlcnktNyIsImEiOiJjazR1YjV4Zjgzbm5tM2tuc2VjemRxa3I5In0.pJit0UFF58qsn--5BoSe0w&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
